e443515d45592a4d5a0da1b6c28c4afc1212c13a
yousef <yousefbaker505@gamil.com>
2026-05-24 16:23:17 +0300

    first commit

--- middleware.ts
+++ middleware.ts
@@ -3,49 +3,26 @@ import type { NextRequest } from "next/server";
 
 const bannedIPs = [
   "1.1.1.1",
-  "8.8.8.8",
 ];
 
 export function middleware(req: NextRequest) {
   const ip =
-    req.ip ||
     req.headers.get("x-forwarded-for") ||
     "Unknown";
 
   const userAgent =
     req.headers.get("user-agent") || "";
 
-  const country =
-    req.geo?.country || "Unknown";
-
   console.log("━━━━━━━━━━━━━━━━━━");
   console.log("🌍 Visitor Joined");
   console.log("IP:", ip);
-  console.log("Country:", country);
   console.log("Device:", userAgent);
   console.log("━━━━━━━━━━━━━━━━━━");
 
-  // Ban IPs
-  if (bannedIPs.includes(ip.toString())) {
+  if (bannedIPs.includes(ip)) {
     return new NextResponse(
-      "🚫 You are banned",
-      {
-        status: 403,
-      }
-    );
-  }
-
-  // Block bots
-  if (
-    userAgent.includes("bot") ||
-    userAgent.includes("crawler") ||
-    userAgent.includes("spider")
-  ) {
-    return new NextResponse(
-      "🤖 Bots blocked",
-      {
-        status: 403,
-      }
+      "🚫 Banned",
+      { status: 403 }
     );
   }
 
